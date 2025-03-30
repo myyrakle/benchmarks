828123c06a4ec150fe0399b0af3a145ad5c1a51c
myyrakle <sssang97@naver.com>
2025-03-31 00:23:30 +0900

    [#2] redis 기본 코드 작성

---
+++ kv_latency/README.md
@@ -0,0 +1,9 @@
+# KV Latency Benchmarks
+
+Key-Value Databases Lantency Benchmarks
+
+## Requirements
+
+```
+rustup default nightly
+```
--- kv_latency/src/main.rs
+++ kv_latency/src/main.rs
@@ -1,3 +1,13 @@
+#![feature(test)]
+extern crate test;
+
+mod redis;
+
+#[bench]
+fn bench_example(b: &mut test::Bencher) {
+    b.iter(|| {});
+}
+
 fn main() {
     println!("Hello, world!");
 }
