60d0a9697d4c7f5663487692b0b3c9e4a64eab19
myyrakle <sssang97@naver.com>
2025-08-31 17:28:09 +0900

    [#32] influxdb v2 벤치마크 추가

--- database_write_performance/src/main.rs
+++ database_write_performance/src/main.rs
@@ -14,7 +14,7 @@ async fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
         eprintln!("Usage: {} <db_type>", args[0]);
-        eprintln!("db_type: postgres, mysql, mongodb, fake");
+        eprintln!("db_type: postgres, mysql, mongodb, scylla, cassandra, influxdb_v2, fake");
         std::process::exit(1);
     }
 
